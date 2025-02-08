874e9bfd53e14cb2e5b90c8a6f158c6f1abea388
SleepyBread <jnroy27@gmail.com>
2025-02-08 13:39:15 -0500

    Creation du projet

---
+++ src/test/java/com/example/highwind/HighWindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.highwind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighWindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
